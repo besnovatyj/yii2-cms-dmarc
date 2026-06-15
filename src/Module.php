7414710c0186b4edc274dec965dc2f3ae91746d1
besnovatyj <metr31@yandex.ru>
2026-06-15 19:55:53 +0500

    modmanNew -> modman

--- src/Module.php
+++ src/Module.php
@@ -9,9 +9,9 @@ declare(strict_types=1);
 namespace Besnovatyj\Dmarc;
 
 use common\components\module\CmsModule;
-use modules\modmanNew\contract\DeclaresModule;
-use modules\modmanNew\contract\ProvidesAdminMenu;
-use modules\modmanNew\contract\ProvidesMigrations;
+use modules\modman\contract\DeclaresModule;
+use modules\modman\contract\ProvidesAdminMenu;
+use modules\modman\contract\ProvidesMigrations;
 use Yii;
 
 /**
